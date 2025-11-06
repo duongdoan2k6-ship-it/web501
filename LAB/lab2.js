ee4a8baabda3b62e34e8021d1f8d14f65313a561
Doan Trong Duong <duongdoan2k6@gmail.com>
2025-11-06 16:39:05 +0700

    Cập nhật LAB1 và LAB2

---
+++ LAB/lab2.js
@@ -0,0 +1,18 @@
+// bai 1
+for (let  i = 0; i < 3; i++) {
+  setTimeout(function () {
+    console.log(i); 
+  }, 100);
+}
+
+// bai 2
+
+const student = {
+  name: "ThuyTien",
+  age: 20,
+};
+
+student.grade = "A";
+student.age = 21;
+
+console.log(student);
